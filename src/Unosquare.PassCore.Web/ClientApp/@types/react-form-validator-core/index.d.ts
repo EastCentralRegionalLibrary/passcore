0d4d7abfabc44216b596b7c07ceb19f0aeddfc52
ECRLib <ecrldev@ecrlib.org>
2025-03-06 09:41:29 -0600

    Use typing, try to fix visual bug
    Rework the declaration and types for react-forms-validator-core

--- src/Unosquare.PassCore.Web/ClientApp/@types/react-form-validator-core/index.d.ts
+++ src/Unosquare.PassCore.Web/ClientApp/@types/react-form-validator-core/index.d.ts
@@ -1,7 +1,40 @@
 declare module 'react-form-validator-core' {
-    export class ValidatorComponent extends React.Component<any> {}
+    import * as React from 'react';
 
-    export class ValidatorForm extends React.Component<any> {
+    export interface ValidatorComponentProps {
+        errorMessages?: string | string[];
+        validators?: string[];
+        value?: any;
+        validatorListener?: (isValid: boolean) => void;
+        withRequiredValidator?: boolean;
+        containerProps?: Record<string, any>;
+    }
+
+    export class ValidatorComponent<P = ValidatorComponentProps, S = {}> extends React.Component<P, S> {
+        validate(value?: any, includeRequired?: boolean, dryRun?: boolean): Promise<boolean>;
+        getErrorMessage(): string;
+        isValid(): boolean;
+        makeInvalid(): void;
+        makeValid(): void;
+    }
+
+    export interface ValidatorFormProps {
+        onSubmit: (event?: React.FormEvent<HTMLFormElement>) => void;
+        instantValidate?: boolean;
+        children?: React.ReactNode;
+        onError?: (errors: any[]) => void;
+        debounceTime?: number;
+        [key: string]: any; //
+    }
+
+    export class ValidatorForm extends React.Component<ValidatorFormProps> {
         public static addValidationRule(ruleName: string, callback: any): void;
+        static getValidator(validator: string, value: any, includeRequired: boolean): Promise<boolean>;
+        attachToForm(component: any): void;
+        detachFromForm(component: any): void;
+        instantValidate: boolean;
+        debounceTime: number;
+        isFormValid: (dryRun?: boolean) => Promise<boolean>;
+        resetValidations: () => void;
     }
 }
