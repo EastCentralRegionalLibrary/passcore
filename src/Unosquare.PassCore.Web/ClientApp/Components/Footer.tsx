6f1c18908344bd1475b4400f3282381a3e6f16e8
ECRLib <ecrldev@ecrlib.org>
2026-04-20 12:24:37 -0500

    fix images and pkg lock

--- src/Unosquare.PassCore.Web/ClientApp/Components/Footer.tsx
+++ src/Unosquare.PassCore.Web/ClientApp/Components/Footer.tsx
@@ -2,10 +2,10 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography/Typography';
 import * as React from 'react';
 import { appVersion } from '../version';
-import mitLogo from '../assets/images/License_icon-mit.svg.png';
-import uslogo from '../assets/images/logo.png';
-import osiLogo from '../assets/images/osi.png';
-import passcoreLogo from '../assets/images/passcore-logo.png';
+import mitLogo from 'url:../assets/images/License_icon-mit.svg.png';
+import uslogo from 'url:../assets/images/logo.png';
+import osiLogo from 'url:../assets/images/osi.png';
+import passcoreLogo from 'url:../assets/images/passcore-logo.png';
 
 export const Footer: React.FC = () => (
     <Box
@@ -18,12 +18,12 @@ export const Footer: React.FC = () => (
             alignItems="center"
         >
             <Box>
-                <img src={passcoreLogo} style={{ marginLeft: '15px', maxWidth: '125px' }} alt="PassCore Logo" />
+                <img src={passcoreLogo.default || passcoreLogo} style={{ marginLeft: '15px', maxWidth: '125px' }} alt="PassCore Logo" />
             </Box>
             <Box>
-                <img src={osiLogo} style={{ margin: '0 10px 0 40px', maxHeight: '30px' }} alt="OSI Logo" />
-                <img src={mitLogo} style={{ marginRight: '10px', maxHeight: '30px' }} alt="MIT Logo" />
-                <img src={uslogo} style={{ maxHeight: '30px' }} alt="US Logo" />
+                <img src={osiLogo.default || osiLogo} style={{ margin: '0 10px 0 40px', maxHeight: '30px' }} alt="OSI Logo" />
+                <img src={mitLogo.default || mitLogo} style={{ marginRight: '10px', maxHeight: '30px' }} alt="MIT Logo" />
+                <img src={uslogo.default || uslogo} style={{ maxHeight: '30px' }} alt="US Logo" />
             </Box>
         </Box>
         <Box
